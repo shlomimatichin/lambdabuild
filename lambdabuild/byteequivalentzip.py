f9982a95aea36c4f4033a5acee13f3d33b5ded20
Shlomi Matichin <shlomomatichin@gmail.com>
2019-05-06 17:40:59 +0300

    bugfix: python3.7 pyc timestamp moved 4 bytes

--- lambdabuild/byteequivalentzip.py
+++ lambdabuild/byteequivalentzip.py
@@ -7,6 +7,9 @@ import contextlib
 
 PYC_TIMESTAMP = datetime.date(1981, 1, 1)
 PYC_UNIX_TIMESTAMP = struct.pack("I", int(time.mktime(PYC_TIMESTAMP.timetuple())))
+PYTHON36_HEADER = b"\x33\x0d\x0d\x0a"
+PYTHON37_HEADER = b"\x42\x0d\x0d\x0a"
+PYTHON27_HEADER = b"\xf3\x03\x0d\x0d"
 
 
 @contextlib.contextmanager
@@ -15,7 +18,12 @@ def createzip(zipfile_path):
         def _add_to_zip(name, contents):
             if name.endswith(".pyc"):
                 assert len(PYC_UNIX_TIMESTAMP) == 4
-                contents = contents[:4] + PYC_UNIX_TIMESTAMP + contents[8:]
+                if contents.startswith(PYTHON37_HEADER):
+                    contents = contents[:8] + PYC_UNIX_TIMESTAMP + contents[12:]
+                else:
+                    assert contents.startswith(PYTHON27_HEADER) or contents.startswith(PYTHON36_HEADER), \
+                            "Unrecognized pyc header, are you using python 2.7, 3.6 or 3.7? %s" % contents[:4]
+                    contents = contents[:4] + PYC_UNIX_TIMESTAMP + contents[8:]
             info = zipfile.ZipInfo(filename=name, date_time=(1980, 1, 1, 0, 0, 0))
             info.external_attr |= (0x1a4 << 16)
             zipper.writestr(info, contents, compress_type=zipfile.ZIP_DEFLATED)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdabuild",
-    version="0.0.2",
+    version="0.0.3",
     author="Shlomi Matichin",
     author_email="shlomomatichin@gmail.com",
     description="Easily build AWS lambda artifacts and layers, correctly",
